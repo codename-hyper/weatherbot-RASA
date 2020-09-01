c8cd6def5a8ed0e273b497ae4300ab5941db42e7
Aadil Aftab Shaik <aadil.aftab.sk@gmail.com>
2020-09-02 04:35:58 +0530

    Update weather_report.py

--- weather_report.py
+++ weather_report.py
@@ -3,7 +3,7 @@ import requests
 
 def weather(city_name):
     city = city_name
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=f5a8c535b5b7f07848e0f4b005a5cd37'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apikey}'
     weather_data = requests.get(url).json()
     weather_main = weather_data['main']
     return weather_main
